1eff75dbf69b2eba9236fe48fd57225c32831e40
satutu <saturnin.gantet@hotmail.fr>
2025-06-28 17:25:24 +0200

    Auto overlay toggle when building transit tube

--- TransitTubeOverLay/Patch.cs
+++ TransitTubeOverLay/Patch.cs
@@ -29,6 +29,37 @@ namespace TransitTube_Overlay_Mod
             }
         }
 
+        /**
+         * Adds auto overlay toggle when selecting building to build.
+         */
+        [HarmonyPatch(typeof(TravelTubeConfig), nameof(TravelTubeConfig.CreateBuildingDef))]
+        public static class TravelTubeConfig_CreateBuildingDef_Patch
+        {
+            public static void Postfix(ref BuildingDef __result)
+            {
+                if (__result == null) return;
+                __result.ViewMode = TransitTubeOverlay.ID;
+            }
+        }
+        [HarmonyPatch(typeof(TravelTubeWallBridgeConfig), nameof(TravelTubeWallBridgeConfig.CreateBuildingDef))]
+        public static class TravelTubeWallBridgeConfig_CreateBuildingDef_Patch
+        {
+            public static void Postfix(ref BuildingDef __result)
+            {
+                if (__result == null) return;
+                __result.ViewMode = TransitTubeOverlay.ID;
+            }
+        }
+        [HarmonyPatch(typeof(TravelTubeEntranceConfig), nameof(TravelTubeEntranceConfig.CreateBuildingDef))]
+        public static class TravelTubeEntrance_CreateBuildingDef_Patch
+        {
+            public static void Postfix(ref BuildingDef __result)
+            {
+                if (__result == null) return;
+                __result.ViewMode = TransitTubeOverlay.ID;
+            }
+        }
+
 
         /**
          * Adds Transit Tube overlay button in overlayMenu
@@ -98,111 +129,112 @@ namespace TransitTube_Overlay_Mod
             }
         }
 
-    }
 
-    /**
-     * Finds the real exits (no bridge)
-     */
-    [HarmonyPatch(typeof(TravelTube), "OnDirtyNavCellUpdated")]
-    public static class TravelTube_OnDirtyNavCellUpdated_Patch
-    {
-        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+
+        /**
+         * Finds the real exits (no bridge)
+         */
+        [HarmonyPatch(typeof(TravelTube), "OnDirtyNavCellUpdated")]
+        public static class TravelTube_OnDirtyNavCellUpdated_Patch
         {
-            LocalBuilder realExitFlag = generator.DeclareLocal(typeof(bool));
-
-            var codes = new List<CodeInstruction>(instructions);
-            var setIsValidExitOnly = AccessTools.Method(typeof(TravelTubeExt), nameof(TravelTubeExt.SetIsValidExitOnly));
-
-            for (int i = 0; i < codes.Count; i++)
+            public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
             {
-                yield return codes[i];
+                LocalBuilder realExitFlag = generator.DeclareLocal(typeof(bool));
 
-                //Init realExitFlag
-                if(
-                    i >= 1 &&
-                    codes[i-1].opcode == OpCodes.Ldc_I4_0 &&
-                    codes[i].opcode == OpCodes.Stloc_3
-                )
+                var codes = new List<CodeInstruction>(instructions);
+                var setIsValidExitOnly = AccessTools.Method(typeof(TravelTubeExt), nameof(TravelTubeExt.SetIsValidExitOnly));
+
+                for (int i = 0; i < codes.Count; i++)
                 {
-                    yield return new CodeInstruction(OpCodes.Ldc_I4_0);
-                    yield return new CodeInstruction(OpCodes.Stloc_S, realExitFlag);
-                }
+                    yield return codes[i];
+
+                    //Init realExitFlag
+                    if (
+                        i >= 1 &&
+                        codes[i - 1].opcode == OpCodes.Ldc_I4_0 &&
+                        codes[i].opcode == OpCodes.Stloc_3
+                    )
+                    {
+                        yield return new CodeInstruction(OpCodes.Ldc_I4_0);
+                        yield return new CodeInstruction(OpCodes.Stloc_S, realExitFlag);
+                    }
 
 
-                //Set Flag
-                if (
-                    i >= 2 &&
-                    codes[i - 2].opcode == OpCodes.Ldfld &&
-                    codes[i - 2].operand is FieldInfo field &&
-                    field.Name == "endNavType" &&
-                    codes[i - 1].opcode == OpCodes.Ldc_I4_8 &&
-                    codes[i].opcode == OpCodes.Beq_S
-                )
-                {
-                    yield return new CodeInstruction(OpCodes.Ldc_I4_1);
-                    yield return new CodeInstruction(OpCodes.Stloc_S, realExitFlag);
-                }
+                    //Set Flag
+                    if (
+                        i >= 2 &&
+                        codes[i - 2].opcode == OpCodes.Ldfld &&
+                        codes[i - 2].operand is FieldInfo field &&
+                        field.Name == "endNavType" &&
+                        codes[i - 1].opcode == OpCodes.Ldc_I4_8 &&
+                        codes[i].opcode == OpCodes.Beq_S
+                    )
+                    {
+                        yield return new CodeInstruction(OpCodes.Ldc_I4_1);
+                        yield return new CodeInstruction(OpCodes.Stloc_S, realExitFlag);
+                    }
 
-                //Update internal State
-                if (
-                    codes[i].opcode == OpCodes.Call &&
-                    codes[i].operand is MethodInfo method &&
-                    method.Name == "UpdateExitStatus" && method.DeclaringType == typeof(TravelTube)
-                )
-                {
-                    yield return new CodeInstruction(OpCodes.Ldarg_0);
-                    yield return new CodeInstruction(OpCodes.Ldloc_S, realExitFlag);
-                    yield return new CodeInstruction(OpCodes.Call, setIsValidExitOnly);
+                    //Update internal State
+                    if (
+                        codes[i].opcode == OpCodes.Call &&
+                        codes[i].operand is MethodInfo method &&
+                        method.Name == "UpdateExitStatus" && method.DeclaringType == typeof(TravelTube)
+                    )
+                    {
+                        yield return new CodeInstruction(OpCodes.Ldarg_0);
+                        yield return new CodeInstruction(OpCodes.Ldloc_S, realExitFlag);
+                        yield return new CodeInstruction(OpCodes.Call, setIsValidExitOnly);
+                    }
                 }
             }
         }
-    }
 
 
-    /**
-     * Updates the bitfieldMap to allow StatusItems to display on our overlay only
-     */
-    [HarmonyPatch(typeof(StatusItem), nameof(StatusItem.GetStatusItemOverlayBySimViewMode))]
-    public static class StatusItem_GetStatusItemOverlayBySimViewMode_Patch
-    {
-        static bool patched = false;
-
-        public static void Prefix()
+        /**
+         * Updates the bitfieldMap to allow StatusItems to display on our overlay only
+         */
+        [HarmonyPatch(typeof(StatusItem), nameof(StatusItem.GetStatusItemOverlayBySimViewMode))]
+        public static class StatusItem_GetStatusItemOverlayBySimViewMode_Patch
         {
-            if (patched) return;
-            patched = true;
+            static bool patched = false;
 
-            var field = typeof(StatusItem).GetField("overlayBitfieldMap", BindingFlags.NonPublic | BindingFlags.Static);
-            if (field == null)
+            public static void Prefix()
             {
-                Debug.LogError("Failed to find overlayBitfieldMap field.");
-                return;
-            }
+                if (patched) return;
+                patched = true;
 
-            var dict = field.GetValue(null) as Dictionary<HashedString, StatusItem.StatusItemOverlays>;
-            if (dict == null)
-            {
-                Debug.LogError("overlayBitfieldMap is null or invalid.");
-                return;
-            }
+                var field = typeof(StatusItem).GetField("overlayBitfieldMap", BindingFlags.NonPublic | BindingFlags.Static);
+                if (field == null)
+                {
+                    Debug.LogError("Failed to find overlayBitfieldMap field.");
+                    return;
+                }
 
-            // Inject overlay mapping
-            dict[TransitTubeOverlay.ID] = (StatusItem.StatusItemOverlays) Constants.statusItemOverlayBit;
+                var dict = field.GetValue(null) as Dictionary<HashedString, StatusItem.StatusItemOverlays>;
+                if (dict == null)
+                {
+                    Debug.LogError("overlayBitfieldMap is null or invalid.");
+                    return;
+                }
+
+                // Inject overlay mapping
+                dict[TransitTubeOverlay.ID] = (StatusItem.StatusItemOverlays)Constants.statusItemOverlayBit;
+            }
         }
-    }
 
-    [HarmonyPatch(typeof(BuildingStatusItems), "CreateStatusItems")]
-    public static class BuildingStatusItem_CreateStatusItems_Patch
-    {
-        public static void Postfix(BuildingStatusItems __instance)
+        [HarmonyPatch(typeof(BuildingStatusItems), "CreateStatusItems")]
+        public static class BuildingStatusItem_CreateStatusItems_Patch
         {
+            public static void Postfix(BuildingStatusItems __instance)
+            {
 
-            __instance.NoTubeConnected.render_overlay = TransitTubeOverlay.ID;
-            __instance.NoTubeExits.render_overlay = TransitTubeOverlay.ID;
-            __instance.TransitTubeEntranceWaxReady.render_overlay = TransitTubeOverlay.ID;
-            __instance.NoTubeConnected.status_overlays |= Constants.statusItemOverlayBit;
-            __instance.NoTubeExits.status_overlays |= Constants.statusItemOverlayBit;
-            __instance.TransitTubeEntranceWaxReady.status_overlays |= Constants.statusItemOverlayBit;
+                __instance.NoTubeConnected.render_overlay = TransitTubeOverlay.ID;
+                __instance.NoTubeExits.render_overlay = TransitTubeOverlay.ID;
+                __instance.TransitTubeEntranceWaxReady.render_overlay = TransitTubeOverlay.ID;
+                __instance.NoTubeConnected.status_overlays |= Constants.statusItemOverlayBit;
+                __instance.NoTubeExits.status_overlays |= Constants.statusItemOverlayBit;
+                __instance.TransitTubeEntranceWaxReady.status_overlays |= Constants.statusItemOverlayBit;
+            }
         }
     }
 }
